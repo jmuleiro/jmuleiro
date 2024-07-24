7733f15d22f60ef937b5e7c11538e47fb8228d88
Joaquin Muleiro <joaquinmuleirobeltran@gmail.com>
2024-07-24 19:04:52 -0300

    use page tokens, get rid of emojis

--- scripts/gmail-lookup/main.py
+++ scripts/gmail-lookup/main.py
@@ -3,8 +3,10 @@ from html.parser import HTMLParser
 import os
 import re
 import json
-import logging
+import time
+import emoji
 import base64
+import logging
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from google.oauth2.credentials import Credentials
@@ -30,12 +32,18 @@ def processData(_d: str, mappings: dict):
         msg = appendMsg(msg, mappings[mp]['data'][0], result[0])
       print(msg)
       return
+  print(f"No mapping for '{_d}'")
 
 class MailParser(HTMLParser):
   """
   Parses Gmail message payloads and drops unnecessary HTML tags and attributes.
   """
   def handle_data(self, data: str):
+    #? Drop all lines that do not contain a dollar sign
+    if not re.findall(r'\$', data):
+      return
+    #? Remove emoji
+    data = emoji.replace_emoji(data, '')
     data = re.sub(r'\D{0,9}(?>\{.*\})|(?>\@\D.*)', '', data)
     #? Remove whitespaces and commas
     data = re.sub(r'\n|\r|\t|\0|,', '', data).strip()
@@ -63,18 +71,37 @@ def main():
     with open(tokenFile, "w") as token:
       token.write(creds.to_json())
   
-  gmailMaxResults = os.getenv("GMAIL_RESULTS_PER_PAGE", "1")
+  gmailMaxResults = os.getenv("GMAIL_RESULTS_PER_PAGE", "100")
   gmailUserId = os.getenv("GMAIL_USER_ID", "me")
+  # Date format: YYYY/MM/DD or MM/DD/YYYY
+  gmailDateFrom = os.getenv("GMAIL_DATE_FROM", "2024/05/31")
+  gmailDateTo = os.getenv("GMAIL_DATE_TO", "2024/07/01")
   gmailQuery = "from: transaction@belo.app"
+  if gmailDateFrom:
+    gmailQuery += f" after:{gmailDateFrom}"
+  if gmailDateTo:
+    gmailQuery += f" before:{gmailDateTo}"
   
   try:
     #? Build Gmail API
     service = build("gmail", "v1", credentials=creds)
-    threads = service.users().threads().list(userId=gmailUserId, maxResults=gmailMaxResults, q=gmailQuery).execute().get("threads", [])
-    for thread in threads:
-      data = (service.users().threads().get(userId=gmailUserId, id=thread["id"])).execute()
-      parser = MailParser()
-      parser.feed(base64.urlsafe_b64decode(data["messages"][0]["payload"]["body"]["data"]).decode())
+    nextPageToken = None
+    while True:
+      response = service.users().threads().list(userId=gmailUserId, maxResults=gmailMaxResults, q=gmailQuery, pageToken=nextPageToken).execute()
+      threads = response.get("threads", [])
+      for thread in threads:
+        data = (service.users().threads().get(userId=gmailUserId, id=thread["id"])).execute()
+        parser = MailParser()
+        parser.feed(base64.urlsafe_b64decode(data["messages"][0]["payload"]["body"]["data"]).decode())
+      
+      nextPageToken = response.get("nextPageToken", None)
+      print(f"NextPageToken: {nextPageToken}")
+      if nextPageToken:
+        sleptSeconds = 1
+        print(f"Sleeping for {sleptSeconds} second(s)...")
+        time.sleep(sleptSeconds)
+      else:
+        break
     
   except HttpError as error:
     print(f"HttpError: {error}")

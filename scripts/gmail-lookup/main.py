feed750c3b30fcbdcda2d50de6b8a0a9ce8f7292
Joaquin Muleiro <joaquinmuleirobeltran@gmail.com>
2024-08-01 09:03:47 -0300

    using mappings and classes

--- scripts/gmail-lookup/main.py
+++ scripts/gmail-lookup/main.py
@@ -15,15 +15,15 @@ from google.oauth2.credentials import Credentials
 from google.auth.transport.requests import Request
 from google_auth_oauthlib.flow import InstalledAppFlow
 
-def init():
-  #? Globals
-  global log
-  global mappings
-  mappings: list[EmailTemplate] = []
+#? Globals
+global log
+global mappings
+mappings: list[EmailTemplate] = []
 
-  #? Setup logging
-  log = getLogger(os.getenv('LOG_LEVEL', 'DEBUG'))
-  
+#? Setup logging
+log = getLogger(os.getenv('LOG_LEVEL', 'DEBUG'))
+
+def init():
   #? Get JSON mappings
   log.debug("Getting mappings...")
   mappingsFile = os.getenv("MAPPINGS_FILE", "scripts/gmail-lookup/mappings.json")
@@ -44,10 +44,13 @@ def init():
   for template in mappingsDict.get("templates"):
     mappings.append(EmailTemplate(template))
   log.debug("Mappings parse successful")
-  exit(0)
 
 #todo: place into classes file, can't separate easily because of logging and mappings
 class MailParser(HTMLParser):
+  def __init__(self, template: EmailTemplate, *, convert_charrefs: bool = True) -> None:
+    super().__init__(convert_charrefs=convert_charrefs)
+    self.template = template
+  
   """
   Parses Gmail message payloads and drops unnecessary HTML tags and attributes.
   """
@@ -60,21 +63,19 @@ class MailParser(HTMLParser):
     data = re.sub(r'\D{0,9}(?>\{.*\})|(?>\@\D.*)', '', data)
     #? Remove whitespaces and commas
     data = re.sub(r'\n|\r|\t|\0|,', '', data).strip()
-    #todo: templates is an array of EmailTemplate
-    templates = EmailTemplate(mappings.get("templates"))
-    self.processData(data, templates)
+    self.processData(data)
   
-  def processData(_d: str, mappings: dict):
+  def processData(self, _d: str):
     def appendMsg(_m: str, _t: str, _M: str) -> str:
       return f"{_m}{_t.title()}: '{_M.strip()}', "
-    for mp in mappings:
-      msg = f"Type: {mp.title()}, "
-      if result := re.findall(mappings[mp]['pattern'], _d):
+    for mp in self.template.mappings:
+      msg = f"Type: {mp.name.title()}, "
+      if result := re.findall(mp.pattern, _d):
         if type(result[0]) == tuple:
           for index, match in enumerate(result[0] if type(result[0]) == tuple else (result[0])):
-            msg = appendMsg(msg, mappings[mp]['data'][index], match)
+            msg = appendMsg(msg, mp.labels[index], match)
         else:
-          msg = appendMsg(msg, mappings[mp]['data'][0], result[0])
+          msg = appendMsg(msg, mp.labels[0], result[0])
         log.debug(msg)
         return
     log.warning(f"No mapping for '{_d}'")
@@ -108,57 +109,58 @@ def main():
       token.write(creds.to_json())
       log.debug(f"Wrote token file to '{tokenFile}'")
 
-  for i, mp in enumerate(mappings):
-    log.debug(f"Template n°{i+1}")
-    gmailQuery = f"from: {mp.sender}"
-    #todo: run mails from mp.sender through mp.mappings
-
   gmailMaxResults = os.getenv("GMAIL_RESULTS_PER_PAGE", "100")
   gmailUserId = os.getenv("GMAIL_USER_ID", "me")
   # Date format: YYYY/MM/DD or MM/DD/YYYY
   gmailDateFrom = os.getenv("GMAIL_DATE_FROM", "2024/05/31")
   gmailDateTo = os.getenv("GMAIL_DATE_TO", "2024/07/01")
-  gmailQuery = "from: transaction@belo.app"
-  if gmailDateFrom:
-    gmailQuery += f" after:{gmailDateFrom}"
-  if gmailDateTo:
-    gmailQuery += f" before:{gmailDateTo}"
-  log.debug(f"""Parameters:
-maxResults: {gmailMaxResults}
-userId: {gmailUserId}
-dateFrom: {gmailDateFrom}
-dateTo: {gmailDateTo}
-query: {gmailQuery}
-""")
-  
+  exitCode = 0
+
   try:
     #? Build Gmail API
     log.info("Building Gmail API...")
     service = build("gmail", "v1", credentials=creds)
-    nextPageToken = None
-    log.debug("Iterating over results...")
-    resultsCount = 0
-    while True:
-      log.debug(f"Results iteration no.: '{resultsCount :+= 1}'")
-      response = service.users().threads().list(userId=gmailUserId, maxResults=gmailMaxResults, q=gmailQuery, pageToken=nextPageToken).execute()
-      threads = response.get("threads", [])
-      for thread in threads:
-        data = (service.users().threads().get(userId=gmailUserId, id=thread["id"])).execute()
-        parser = MailParser()
-        parser.feed(base64.urlsafe_b64decode(data["messages"][0]["payload"]["body"]["data"]).decode())
-      
-      nextPageToken = response.get("nextPageToken", None)
-      log.debug(f"NextPageToken: {nextPageToken}")
-      if nextPageToken:
-        sleptSeconds = 1
-        log.info(f"Sleeping for {sleptSeconds} second(s)...")
-        time.sleep(sleptSeconds)
-      else:
-        log.info("Got to the end of the list")
-        break
+
+    log.debug(f"Iterating over {len(mappings)} templates")
+    for i, mp in enumerate(mappings):
+      log.debug(f"Template n°{i+1}")
+      gmailQuery = f"from: {mp.sender}"
+      if gmailDateFrom:
+        gmailQuery += f" after:{gmailDateFrom}"
+      if gmailDateTo:
+        gmailQuery += f" before:{gmailDateTo}"
+        log.debug(f"Parameters: maxResults: '{gmailMaxResults}', userId: '{gmailUserId}', dateFrom: '{gmailDateFrom}', dateTo: '{gmailDateTo}', query: '{gmailQuery}'")
     
-  except HttpError as error:
-    log.critical(f"HttpError: {error}")
+      nextPageToken = None
+      log.debug("Iterating over results...")
+      resultsCount = 0
+      while True:
+        log.debug(f"Results iteration no.: '{resultsCount :+= 1}'")
+        response = service.users().threads().list(userId=gmailUserId, maxResults=gmailMaxResults, q=gmailQuery, pageToken=nextPageToken).execute()
+        threads = response.get("threads", [])
+        for thread in threads:
+          data = (service.users().threads().get(userId=gmailUserId, id=thread["id"])).execute()
+          parser = MailParser(mp)
+          parser.feed(base64.urlsafe_b64decode(data["messages"][0]["payload"]["body"]["data"]).decode())
+
+        nextPageToken = response.get("nextPageToken", None)
+        log.debug(f"NextPageToken: {nextPageToken}")
+        if nextPageToken:
+          sleptSeconds = 1
+          log.info(f"Sleeping for {sleptSeconds} second(s)...")
+          time.sleep(sleptSeconds)
+        else:
+          log.info("Got to the end of the list")
+          break
+    
+  except HttpError as e:
+    log.critical(f"HttpError: {e}")
+    exitCode = 1
+  except Exception as e:
+    log.critical(f"{e.__class__.__name__}: {e}")
+    exitCode = 1
+  finally:
+    exit(exitCode)
 
 if __name__ == "__main__":
   init()

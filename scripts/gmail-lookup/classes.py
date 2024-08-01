b010cedb7c2c76987679793c90dadba6f84afb50
Joaquin Muleiro <joaquinmuleirobeltran@gmail.com>
2024-08-01 09:09:07 -0300

    put MailParser in classes file

--- scripts/gmail-lookup/classes.py
+++ scripts/gmail-lookup/classes.py
@@ -1,3 +1,11 @@
+import os
+import re
+import emoji
+import logging as log
+from html.parser import HTMLParser
+#? Setup logging
+#log = getLogger(os.getenv('LOG_LEVEL', 'DEBUG'))
+
 class EmailTemplate:
   """
   Email template class, which contain metric mappings
@@ -18,3 +26,37 @@ class MetricMapping:
     self.description = mapping.get("description", "Default Metric Description")
     self.metric = mapping.get("metric")
     self.labels = mapping.get("labels")
+
+class MailParser(HTMLParser):
+  def __init__(self, template: EmailTemplate, *, convert_charrefs: bool = True) -> None:
+    super().__init__(convert_charrefs=convert_charrefs)
+    self.template = template
+  
+  """
+  Parses Gmail message payloads and drops unnecessary HTML tags and attributes.
+  """
+  def handle_data(self, data: str):
+    #? Drop all lines that do not contain a dollar sign
+    if not re.findall(r'\$', data):
+      return
+    #? Remove emoji
+    data = emoji.replace_emoji(data, '')
+    data = re.sub(r'\D{0,9}(?>\{.*\})|(?>\@\D.*)', '', data)
+    #? Remove whitespaces and commas
+    data = re.sub(r'\n|\r|\t|\0|,', '', data).strip()
+    self.processData(data)
+  
+  def processData(self, _d: str):
+    def appendMsg(_m: str, _t: str, _M: str) -> str:
+      return f"{_m}{_t.title()}: '{_M.strip()}', "
+    for mp in self.template.mappings:
+      msg = f"Type: {mp.name.title()}, "
+      if result := re.findall(mp.pattern, _d):
+        if type(result[0]) == tuple:
+          for index, match in enumerate(result[0] if type(result[0]) == tuple else (result[0])):
+            msg = appendMsg(msg, mp.labels[index], match)
+        else:
+          msg = appendMsg(msg, mp.labels[0], result[0])
+        log.debug(msg)
+        return
+    log.warning(f"No mapping for '{_d}'")
--- scripts/gmail-lookup/main.py
+++ scripts/gmail-lookup/main.py
@@ -1,14 +1,11 @@
 # Ref https://docs.python.org/3/library/html.parser.html
 import os
-import re
 import json
 import time
-import emoji
 import base64
 from logger import getLogger
 from jsonschema import validate
-from classes import EmailTemplate
-from html.parser import HTMLParser
+from classes import EmailTemplate, MailParser
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from google.oauth2.credentials import Credentials
@@ -44,41 +41,6 @@ def init():
   for template in mappingsDict.get("templates"):
     mappings.append(EmailTemplate(template))
   log.debug("Mappings parse successful")
-
-#todo: place into classes file, can't separate easily because of logging and mappings
-class MailParser(HTMLParser):
-  def __init__(self, template: EmailTemplate, *, convert_charrefs: bool = True) -> None:
-    super().__init__(convert_charrefs=convert_charrefs)
-    self.template = template
-  
-  """
-  Parses Gmail message payloads and drops unnecessary HTML tags and attributes.
-  """
-  def handle_data(self, data: str):
-    #? Drop all lines that do not contain a dollar sign
-    if not re.findall(r'\$', data):
-      return
-    #? Remove emoji
-    data = emoji.replace_emoji(data, '')
-    data = re.sub(r'\D{0,9}(?>\{.*\})|(?>\@\D.*)', '', data)
-    #? Remove whitespaces and commas
-    data = re.sub(r'\n|\r|\t|\0|,', '', data).strip()
-    self.processData(data)
-  
-  def processData(self, _d: str):
-    def appendMsg(_m: str, _t: str, _M: str) -> str:
-      return f"{_m}{_t.title()}: '{_M.strip()}', "
-    for mp in self.template.mappings:
-      msg = f"Type: {mp.name.title()}, "
-      if result := re.findall(mp.pattern, _d):
-        if type(result[0]) == tuple:
-          for index, match in enumerate(result[0] if type(result[0]) == tuple else (result[0])):
-            msg = appendMsg(msg, mp.labels[index], match)
-        else:
-          msg = appendMsg(msg, mp.labels[0], result[0])
-        log.debug(msg)
-        return
-    log.warning(f"No mapping for '{_d}'")
 
 def main():
   log.debug("Getting credentials...")

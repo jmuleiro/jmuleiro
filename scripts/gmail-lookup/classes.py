3a5242126a0dd678a1555982df4e8bb08e8b9807
Joaquin Muleiro <joaquinmuleirobeltran@gmail.com>
2024-08-02 09:25:15 -0300

    wip prometheus metrics

--- scripts/gmail-lookup/classes.py
+++ scripts/gmail-lookup/classes.py
@@ -2,6 +2,8 @@ import re
 import emoji
 import logging as log
 from html.parser import HTMLParser
+from typing import Union
+from prometheus_client import Counter, Gauge, Summary, Histogram, Info, Enum
 
 class EmailTemplate:
   """
@@ -21,13 +23,46 @@ class MetricMapping:
     self.pattern = mapping.get("pattern")
     self.name = mapping.get("name", "Default Metric Name")
     self.description = mapping.get("description", "Default Metric Description")
-    self.metric = mapping.get("metric")
     self.labels = mapping.get("labels")
+    self.metricName = mapping.get("metric").get("name")
+    #todo: add schema properties for metric attributes 
+    #todo: i.e. namespace, unit, etc.
+    #todo: add states for enum type
+    match mapping.get("metric").get("type", "gauge"):
+      case "counter":
+        self.metric = Counter(mapping.get("metric").get("name"), 
+                              self.description, 
+                              labelnames=self.labels)
+      case "summary":
+        self.metric = Summary(mapping.get("metric").get("name"),
+                              self.description,
+                              labelnames=self.labels)
+      case "histogram":
+        self.metric = Histogram(mapping.get("metric").get("name"),
+                              self.description,
+                              labelnames=self.labels)
+      case "info":
+        self.metric = Info(mapping.get("metric").get("name"),
+                              self.description,
+                              labelnames=self.labels)
+      case "enum":
+        self.metric = Enum(mapping.get("metric").get("name"),
+                              self.description,
+                              labelnames=self.labels)
+      case "gauge" | _:
+        self.metric = Gauge(mapping.get("metric").get("name"), 
+                            self.description,
+                            labelnames=self.labels)
 
 class MailParser(HTMLParser):
   def __init__(self, template: EmailTemplate, *, convert_charrefs: bool = True) -> None:
     super().__init__(convert_charrefs=convert_charrefs)
     self.template = template
+    
+    #? Internal metrics
+    self.errorsCount = Counter('mail_exporter_errors', 
+                               "Number of mail exporter errors",
+                               ("name", "description", "metric"))
   
   """
   Parses Gmail message payloads and drops unnecessary HTML tags and attributes.
@@ -50,10 +85,31 @@ class MailParser(HTMLParser):
       msg = f"Type: {mp.name.title()}, "
       if result := re.findall(mp.pattern, _d):
         if type(result[0]) == tuple:
-          for index, match in enumerate(result[0] if type(result[0]) == tuple else (result[0])):
+          for index, match in enumerate(result[0]):
             msg = appendMsg(msg, mp.labels[index], match)
         else:
           msg = appendMsg(msg, mp.labels[0], result[0])
+        #self.processMetric(result[0], mp)
         log.debug(msg)
         return
     log.warning(f"No mapping for '{_d}'")
+  
+  def processMetric(self, result: Union[tuple|str], mapping: MetricMapping):
+    log.debug(f"Processing metric '{mapping.metricName}' type '{type(mapping.metric)}' with values: {result}")
+    if (len(mapping.labels) == 1 and type(result) == tuple) or (len(mapping.labels) != 1 and type(result) == str):
+      log.error(f"Labels and capturing groups mismatch: Got {len(mapping.labels)} labels and {1 if type(result) != tuple else len(result)} capturing group")
+      self.errorsCount.labels(name=mapping.metricName, description=mapping.description, metric=mapping.metric._name)
+      return
+    match mapping.metric.__qualname__:
+      case Counter.__qualname__:
+        pass
+      case Summary.__qualname__:
+        pass
+      case Histogram.__qualname__:
+        pass
+      case Info.__qualname__:
+        pass
+      case Enum.__qualname__:
+        pass
+      case Gauge.__qualname__ | _:
+        pass
